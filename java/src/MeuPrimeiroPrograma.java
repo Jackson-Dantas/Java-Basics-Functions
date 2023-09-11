8ef0c4749a85bb4094b1d3c006e082ae0572e08d
Jackson-Dantas <jacsdaniel40@gmail.com>
2023-09-11 19:17:31 -0300

    Upload my code

---
+++ java/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,38 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) throws Exception {
+
+    // barra-barra em java serve para fazer comentários no código
+    // o método "main" é o ponto de entrada e ignação do programa
+    // variáveis são espaços de memória alocado e representados por tipo
+    // tipos de dados (variáveis): Strings, Int., Double (float) and Bolean (false or true)
+
+        var Jesus = "ola";
+        System.out.println(Jesus);
+    
+    // no JAVA moderno para se declarar uma varíavel basta escreve var (nome da Variável) e atribuir o valor (=) como feito acima
+         
+        Jesus = "Oi, eu sou o Goku!";
+         System.out.println(Jesus);
+       
+
+    //Após declarada a variável é possível mudar o conteúdo dessa variável chamando esse variável e atribuindo um novo valor, sem pecisar declarar VAR novamente
+ } 
+
+   //**************************Criação de função no JAVA**********************
+   // Criação de função se faz primeiro pela declaração padrão: static void <nome da func>
+
+        static void sum (int x, int y) {
+
+         System.out.println( x + y);
+   
+   //  sum > é o nome que damos a função, mas pode ser qualquer nome, de preferência um verbo que indique o que a função faz
+   // os parenteses recebem os parâmetros que a função irá receber, são os dados que a função irá trabalhar.
+   // Parâmetros: são declarados dentro dos parenteses, e devem ser declarados o tipo de dado (intenger, float...) em seguida o nome que pode ser qualquer um no caso dessa função é X e Y
+   // {} o bloco de chaves recebe o código que será executado
+   
+        }
+ 
+
+
+}
