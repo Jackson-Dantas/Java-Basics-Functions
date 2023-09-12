6832f197288b6a920edac5f1c488b5fb7b0cdb51
Jackson-Dantas <jacsdaniel40@gmail.com>
2023-09-12 19:26:51 -0300

    Upload basic code conception

---
+++ funcao/src/funcao.java
@@ -0,0 +1,11 @@
+public class funcao {
+    public static void main(String[] args) throws Exception {
+      sum(15,20); //argumentos 
+    }
+
+static void sum(int x, int y) {
+
+    System.out.println(x+y);
+}
+    
+}
